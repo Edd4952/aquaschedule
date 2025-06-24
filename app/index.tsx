8177a45cffb194ef9a5d6c16c84ea616f484bb08
Edd4952 <zilbert3dward@gmail.com>
2025-06-23 22:06:05 -0500

    test for main pc

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>test.</Text>
       <Image
         source={{
           uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F443446%2Fpexels-photo-443446.jpeg%3Fcs%3Dsrgb%26dl%3Ddaylight-forest-glossy-443446.jpg%26fm%3Djpg&f=1&ipt=277bd0f837b4737f2d0eff0600848fafaaa4969c15472590deb08b1eb7cfa813",
